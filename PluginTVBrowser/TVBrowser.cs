65d29dbe875b464024ffa7e35c217285ce885066
Philipp Kapfer <Crazyphil@users.noreply.github.com>
2013-03-14 19:34:38 +0000

    Umstellung auf API(-Fehlermeldungen) statt TVM2OTR - liefert detailliertere Fehler Bestimmen des passenden Installationspfads für das TV-Browser-Plugin (Adminrechte?) und Umstellung auf das neue Plugin Joda-Time anstelle irgendeiner komischen DateTime-Klasse im Java-TV-Browser-Plugin Deaktivieren des OTR-Remote-Eintrags in TV-Browser anstelle des Entfernens (Usability) Programmbaum-Unterstützung zum Anzeigen aller von OTR Remote aufgenommenen Sendungen Genre-Parameterübergabe Ausführen von OTR Remote in eigenem Thread, damit TV-Browser bedienbar bleibt Unterstützung von Pfaden in Anführungszeichen und/oder Mono beim Prüfen der Validität Unterstützung von TV-Browser 2.7 für HTML-Hilfetext in den Einstellungen

--- PluginTVBrowser/TVBrowser.cs
+++ PluginTVBrowser/TVBrowser.cs
@@ -95,8 +95,26 @@ namespace Crazysoft.OTRRemote
                         }
                         PluginInterop.WriteDebugLog("Enable()", String.Concat("Found registry path \"HKEY_LOCAL_MACHINE\\SOFTWARE\\", subkey, "\""));
 
-                        SetPathSetting(path);
+                        try
+                        {
+                            FileStream fs = new FileStream(Path.Combine(path, "access.tmp"), FileMode.Create, FileAccess.Write);
+                            fs.Close();
+                            fs.Dispose();
+                            File.Delete(Path.Combine(rk.GetValue("InstallDir").ToString(), "access.tmp"));
+                        }
+                        catch (UnauthorizedAccessException)
+                        {
+                            if (tvbrowserVersion.Major < 3)
+                            {
+                                path = String.Format("{0}{1}TV-Browser{1}{2}", Environment.GetEnvironmentVariable("USERPROFILE"), Path.DirectorySeparatorChar, tvbrowserVersion.ToString());
+                            }
+                            else
+                            {
+                                path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), tvbrowserVersion.ToString());
+                            }
+                        }
 
+                        SetPathSetting(path);
                         foundTvBrowser = true;
                     }
                 }
@@ -159,23 +177,6 @@ namespace Crazysoft.OTRRemote
                 try
                 {
                     File.Copy("OTRRemote.jar", Path.Combine(Path.Combine(settings[Lang.TVBrowser.Plugin_Settings_Path].Value, "plugins"), "OTRRemote.jar"), true);
-                    StreamWriter sw = new StreamWriter(Path.Combine(Path.Combine(settings[Lang.TVBrowser.Plugin_Settings_Path].Value, "plugins"), "OTRRemote.jar.config"));
-                    
-                    // When running on the Mono Framework, add "mono" before the filename
-                    if (Type.GetType("Mono.Runtime") != null)
-                    {
-                        sw.Write("mono ");
-                    }
-
-                    string otrpath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "OTRRemote.exe");
-                    // If the user has given a relative path to TV-Browser (= portable), also add a relative path to OTR Remote
-                    if (!Path.IsPathRooted(settings[Lang.TVBrowser.Plugin_Settings_Path].Value) || settings[Lang.TVBrowser.Plugin_Settings_Path].Value[0] == Path.DirectorySeparatorChar)
-                    {
-                        otrpath = RelativePathTo(Path.GetFullPath(settings[Lang.TVBrowser.Plugin_Settings_Path].Value), otrpath);
-                    }
-
-                    sw.WriteLine(otrpath);
-                    sw.Close();
                 }
                 catch (UnauthorizedAccessException)
                 {
@@ -208,80 +209,6 @@ namespace Crazysoft.OTRRemote
         {
             // Check, if the entered TV-Browser installation path exists
             return Directory.Exists(Path.Combine(path, "plugins"));
-        }
-
-        /// <summary>
-        /// Creates a relative path from one file
-        /// or folder to another.
-        /// </summary>
-        /// <param name="fromDirectory">
-        /// Contains the directory that defines the 
-        /// start of the relative path.
-        /// </param>
-        /// <param name="toPath">
-        /// Contains the path that defines the
-        /// endpoint of the relative path.
-        /// </param>
-        /// <returns>
-        /// The relative path from the start
-        /// directory to the end path.
-        /// </returns>
-        /// <see cref="http://weblogs.asp.net/pwelter34/archive/2006/02/08/create-a-relative-path-code-snippet.aspx"/>
-        /// <exception cref="ArgumentNullException"></exception>
-        private static string RelativePathTo(string fromDirectory, string toPath)
-        {
-            if (fromDirectory == null)
-                throw new ArgumentNullException("fromDirectory");
-
-            if (toPath == null)
-                throw new ArgumentNullException("toPath");
-
-            bool isRooted = Path.IsPathRooted(fromDirectory) && Path.IsPathRooted(toPath);
-
-            if (isRooted)
-            {
-                bool isDifferentRoot = string.Compare(Path.GetPathRoot(fromDirectory), Path.GetPathRoot(toPath), true) != 0;
-
-                if (isDifferentRoot)
-                    return toPath;
-            }
-
-            System.Collections.Specialized.StringCollection relativePath = new System.Collections.Specialized.StringCollection();
-            string[] fromDirectories = fromDirectory.Split(Path.DirectorySeparatorChar);
-
-            string[] toDirectories = toPath.Split(Path.DirectorySeparatorChar);
-
-            int length = Math.Min(fromDirectories.Length, toDirectories.Length);
-
-            int lastCommonRoot = -1;
-
-            // find common root
-            for (int x = 0; x < length; x++)
-            {
-                if (string.Compare(fromDirectories[x], toDirectories[x], true) != 0)
-                    break;
-
-                lastCommonRoot = x;
-            }
-            if (lastCommonRoot == -1)
-                return toPath;
-
-            // add relative folders in from path
-            for (int x = lastCommonRoot + 1; x < fromDirectories.Length; x++)
-                if (fromDirectories[x].Length > 0)
-                    relativePath.Add("..");
-
-            // add to folders to path
-            for (int x = lastCommonRoot + 1; x < toDirectories.Length; x++)
-                relativePath.Add(toDirectories[x]);
-
-            // create relative path
-            string[] relativeParts = new string[relativePath.Count];
-            relativePath.CopyTo(relativeParts, 0);
-
-            string newPath = string.Join(Path.DirectorySeparatorChar.ToString(), relativeParts);
-
-            return newPath;
         }
     }
 }
