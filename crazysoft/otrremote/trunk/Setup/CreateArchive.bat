0e569457639b6b76c76ff543b38d402bfeaad64c
crazysoft <crazysoft@c1eb9856-8af9-4821-ac13-783a885f5138>
2017-04-18 13:24:23 +0000

    Add TV-Browser JAR to TAR release

--- crazysoft/otrremote/trunk/Setup/CreateArchive.bat
+++ crazysoft/otrremote/trunk/Setup/CreateArchive.bat
@@ -2,6 +2,7 @@
 cd /D "F:\Programmierung\PiKaSoft OTR Remote\PiKaSoft OTR Remote\bin\Release"
 echo Erstelle gepacktes Archiv...
 "F:\Programmierung\AppUpdate\Crazysoft AppUpdate Builder\bin\Release\7za.exe" a -ttar -r -xr!.svn\ -xr!*.pdb -xr!*.vshost.exe* "F:\Programmierung\PiKaSoft OTR Remote\Setup\Output\cotrremote.tar" *
+"F:\Programmierung\AppUpdate\Crazysoft AppUpdate Builder\bin\Release\7za.exe" a -ttar -r "F:\Programmierung\PiKaSoft OTR Remote\Setup\Output\cotrremote.tar" "F:\Programmierung\PiKaSoft OTR Remote\TVBrowserPlugin\bin\OTRRemote.jar"
 cd /D "F:\Programmierung\PiKaSoft OTR Remote\Setup\Output"
 "F:\Programmierung\AppUpdate\Crazysoft AppUpdate Builder\bin\Release\7za.exe" a -tgzip -mx9 -mfb258 cotrremote.tar.gz cotrremote.tar
 del cotrremote.tar
